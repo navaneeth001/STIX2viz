60915cc9ae4e3b5bc9acf8a231f39a208ab40c00
navaneeth001 <navaneethpqln@gmail.com>
2024-07-05 20:09:21 +0530

    Feat added new prop onNodeclick and code reafactor, removed sample Json

--- src/Stixviewer/Stixviewer.js
+++ src/Stixviewer/Stixviewer.js
@@ -1,52 +1,69 @@
-import React, { useRef, useEffect, useMemo } from "react";
+import React, { useRef, useEffect } from "react";
 import stix2viz from "../stix2viz/stix2viz/stix2viz";
 import PropTypes from "prop-types";
 
 let graphView = null;
 
-const Stixviewer = ({ stixJson = [], graphStyle, wrapStyle, onNodeClick }) => {
+const Stixviewer = ({
+  stixJson = null,
+  graphStyle,
+  wrapStyle,
+  onNodeclick = () => {},
+}) => {
   const graphContainer = useRef(null);
-  const customConfig = useMemo(() => ({ iconDir: "." }), []);
+  const customConfig = {};
 
   function graphViewClickHandler(event, edgeDataSet, stixIdToObject) {
     if (event.nodes.length > 0) {
       // Handle node click event if needed
+      if (onNodeclick) onNodeclick();
     }
   }
 
   useEffect(() => {
-    if (graphContainer.current) {
-      const graphWrapper =
-        graphContainer.current.querySelector("#graphContainer");
-      const [nodeDataList, edgeDataSet, stixIdToObject] =
-        stix2viz.makeGraphData(stixJson);
-
-      graphView = stix2viz.makeGraphView(
-        graphWrapper,
-        nodeDataList,
-        edgeDataSet,
-        stixIdToObject,
-        customConfig
-      );
-      graphView.on("click", (e) =>
-        graphViewClickHandler(e, edgeDataSet, stixIdToObject)
-      );
+    if (graphContainer.current && stixJson) {
+      try {
+        const graphWrapper =
+          graphContainer.current.querySelector("#graphContainer");
+        const [nodeDataList, edgeDataSet, stixIdToObject] =
+          stix2viz.makeGraphData(stixJson);
+        customConfig.iconDir = ".";
+        graphView = stix2viz.makeGraphView(
+          graphWrapper,
+          nodeDataList,
+          edgeDataSet,
+          stixIdToObject,
+          customConfig
+        );
+        graphView.on("click", (e) =>
+          graphViewClickHandler(e, edgeDataSet, stixIdToObject)
+        );
+      } catch (error) {
+        console.error("Failed to create graph view:", error);
+        // Optionally, you can display a user-friendly message in the UI
+      }
     }
-  }, [stixJson, customConfig]);
+  }, [stixJson]);
 
-  return (
-    <div ref={graphContainer}>
-      <div style={{ ...wrapStyle }}>
-        <div
-          id="graphContainer"
-          style={{
-            width: 600,
-            height: 600,
-            ...graphStyle,
-          }}
-        />
-      </div>
-    </div>
+  return React.createElement(
+    "div",
+    { ref: graphContainer },
+    React.createElement(
+      "div",
+      {
+        style: {
+          ...wrapStyle,
+        },
+      },
+      React.createElement("div", {
+        id: "graphContainer",
+        style: {
+          width: 600,
+          height: 600,
+          ...graphStyle,
+        },
+      })
+    )
   );
 };
 
@@ -56,11 +73,4 @@ Stixviewer.propTypes = {
   wrapStyle: PropTypes.object,
   onNodeClick: PropTypes.func,
 };
-
-Stixviewer.defaultProps = {
-  stixJson: [],
-  graphStyle: {},
-  wrapStyle: {},
-};
-
 export default Stixviewer;
